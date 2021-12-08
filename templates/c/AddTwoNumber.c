596a1e5857e2de457d0adc84cb35d5b2429ab502
zhuangll <liuzhuang0410@163.com>
2021-12-08 21:41:56 +0800

    zlog

--- templates/c/AddTwoNumber.c
+++ templates/c/AddTwoNumber.c
@@ -3,17 +3,8 @@
 
 int addTwoNumber(int a, int b)
 {
-    int rc;
-    zlog_category_t *zc;
-	//rc = dzlog_init("test_default.conf", "my_cat");
-	rc = dzlog_init("use_zlog.conf", "my_cat");
-	//rc = dzlog_init(NULL, "my_cat");
-	if (rc) {
-		printf("init failed\n");
-		return -1;
-	}
-    
-    zc = zlog_get_category("my_cat");
+
+    zlog_category_t *zc = zlog_get_category("my_cat");
 	if (!zc) {
 		printf("get cat fail\n");
 		zlog_fini();
@@ -22,8 +13,6 @@ int addTwoNumber(int a, int b)
     //zlog_info(rc, "hello zlog");
     zlog_info(zc, "hello zlog.\r");
 	dzlog_info("hello, zlog.haha.\r");
-
-	zlog_fini();
 	
     int c = a + b;
     return c;
--- templates/c/main.c
+++ templates/c/main.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
 #include"AddTwoNumber.h"
+#include "zlog.h"
+
+int initZlog()
+{
+	int rc;
+	//rc = dzlog_init("test_default.conf", "my_cat");
+	rc = dzlog_init("use_zlog.conf", "my_cat");
+	//rc = dzlog_init(NULL, "my_cat");
+	if (rc) {
+		printf("init failed\n");
+		return -1;
+	}
+    return 0;
+}
 
 int main()
 {
+	initZlog();
+
+	zlog_category_t *zc = zlog_get_category("my_cat");
 	printf("Hello World!\n");
 	int a = 2;
 	int b = 3;
 	int c = addTwoNumber(a, b);
-	printf("c = %d", c);
+	printf("c = %d\n", c);
+	
+	zlog_info(zc, "hello zlog.\r");
+	
+	zlog_fini();
 	return (0);
 }
