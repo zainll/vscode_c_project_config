02cae4ed0a7f0cabb2ed6bf9a275f0901bae3113
zhuangll <liuzhuang0410@163.com>
2021-12-10 23:45:55 +0800

    gtest

--- templates/cpp_tests/Test.cpp
+++ templates/cpp_tests/Test.cpp
@@ -1,13 +1,27 @@
-#include <gmock/gmock.h>
-#include <gtest/gtest.h>
+//#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "AddTwoNumber.h"
 
-using ::testing::Eq;
+/* using ::testing::Eq;
 using ::testing::Return;
 
 TEST(lib, Case2)
 {
-    ///int n = testHelloWorld();
-    //int n = HelloWorld();
     int n = 3; 
     EXPECT_EQ(n, 3);
+}
+ */
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(testTest, add)
+{
+    int res = addTwoNumber(3, 2);
+    //int res = 5;
+    ASSERT_EQ(res, 5);
+   // ASSERT_NEAR(res, 5):
 }
